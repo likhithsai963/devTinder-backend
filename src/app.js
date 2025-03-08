3494ed3059a4452877298f9b07bf5817c7077c1d
likhithsai963 <likhithsai963@gmail.com>
2025-03-08 12:34:05 +0530

    playing with route handlers

--- src/app.js
+++ src/app.js
@@ -11,6 +11,22 @@ app.post("/user",(req,res)=>{
     res.send("Data saved Successfully")
 })
 
+// app.use("/user",[(req,res,next) =>{
+//     // res.send("Response 1")
+//     next();
+// },(req,res)=>{
+//     res.send("Response2")
+// }])
+
+app.use("/user",[(req,res,next) =>{
+    // res.send("Response 1")
+    next();
+},(req,res,next)=>{
+    // res.send("Response2")
+    next();
+}],(req,res,next)=>{
+    res.send("Response 3")
+})
 app.listen(3000,()=>{
     console.log("successfully listening to port 3000")
 });
