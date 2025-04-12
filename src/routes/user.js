e44a9cb473be890aa347aec60d1ec4e023f15c6c
likhithsai963 <likhithsai963@gmail.com>
2025-04-12 15:11:10 +0530

    connections fix

--- src/routes/user.js
+++ src/routes/user.js
@@ -19,7 +19,7 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
         res.json({ message: "Data Fetched successfully", data: connectionRequest })
     } catch (err) {
 
-        res.status(400).json({message : err.message})
+        res.status(400).json({ message: err.message })
 
     }
 });
@@ -27,24 +27,30 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
 userRouter.get("/user/connections", userAuth, async (req, res) => {
     try {
         const loggedInUser = req.user;
-        const connectionRequest = await ConnectionRequest.find({
+
+        const connectionRequests = await ConnectionRequest.find({
             $or: [
                 { toUserId: loggedInUser._id, status: "accepted" },
                 { fromUserId: loggedInUser._id, status: "accepted" },
-            ]
+            ],
         })
-        const data = connectionRequest.map((row) => {
-            if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
-                return row.toUserId
-            }
-            return row.fromUserId
-        })
+            .populate("fromUserId", USER_SAFE_DATA)
+            .populate("toUserId", USER_SAFE_DATA);
 
-        res.json({ data })
-    } catch (error) {
-        res.status(400).json({ message: error.message })
+        console.log(connectionRequests);
+
+        const data = connectionRequests.map((row) => {
+            if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+                return row.toUserId;
+            }
+            return row.fromUserId;
+        });
+
+        res.json({ data });
+    } catch (err) {
+        res.status(400).send({ message: err.message });
     }
-})
+});
 
 userRouter.get("/feed", userAuth, async (req, res) => {
     try {
